b008bac52f9782ee435db0084ae93206b3a82ad2
hzlshen <m15277968105@163.com>
2017-12-12 16:33:19 +0800

    重写tab 不用cli的

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,27 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {HomePage} from "../pages/home/home";
 
-import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
-  templateUrl: 'app.html'
+  // templateUrl: 'app.html'
+  template:`
+    <ion-tabs>
+      <ion-tab tabIcon="body" tabTitle="学员" [root]="tab1"></ion-tab>
+      <ion-tab tabIcon="settings" tabTitle="设置" [root]="tab2"></ion-tab>
+    </ion-tabs>`
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  // rootPage:any = HomePage;
+  tab1:any;
+  tab2:any;
+  
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    
+    this.tab1=HomePage;
+    
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,10 +11,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -26,10 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
   ],
   providers: [
     StatusBar,
